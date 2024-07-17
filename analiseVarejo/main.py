5e58e75dda2ee9a7ee2a3a467b2f4246c7c59d0c
Everson Esteves <esteveseverson@gmail.com>
2024-07-17 17:52:10 -0300

    projeto analise varejo
    pergunta 4

--- analiseVarejo/main.py
+++ analiseVarejo/main.py
@@ -38,12 +38,28 @@ def pergunta3(df):
     #plota os dados
     plt.figure(figsize=(18, 5))
     plt.bar(agrupamento['Estado'], agrupamento['Valor_Venda'], color='green')
-    plt.title('Total de Vendas por Data')
+    plt.title('Total de Vendas por Estado')
     plt.xlabel('Estado')
     plt.xticks(rotation=90) #melhor visualização
     plt.ylabel('Valor total ($)')
     plt.grid(axis='y')
     plt.tight_layout()
+
+    plt.show()
+
+def pergunta4(df):
+    #selecionando o agrupamento e selecionando os 10 maiores [ordenando e pegando os 10 primerios]
+    agrupamento = df.groupby('Cidade')['Valor_Venda'].sum().reset_index()
+    maiores_vendas = agrupamento.sort_values(by='Valor_Venda', ascending=False).head(10)
+
+    #plotando o grafico de barras
+    plt.figure(figsize=(11, 8))
+    plt.bar(maiores_vendas['Cidade'], maiores_vendas['Valor_Venda'], width=0.5, color='green')
+    plt.title('Top 10 cidades com maiores vendas')
+    plt.xlabel('Cidade')
+    plt.xticks(rotation=45) #melhor visualização
+    plt.ylabel('Valor total ($)')
+    plt.grid(axis='y')
 
     plt.show()
 
@@ -58,4 +74,5 @@ print(df.isnull().sum())
 
 # pergunta1(data)
 # pergunta2(data)
-pergunta3(data)
+# pergunta3(data)
+pergunta4(data)
