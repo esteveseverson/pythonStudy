82ceb0cee2b130e69800e9f25abf2dc10801abed
Everson Esteves <esteveseverson@gmail.com>
2024-07-17 17:38:20 -0300

    projeto analise varejo
    pergunta 3

--- analiseVarejo/main.py
+++ analiseVarejo/main.py
@@ -8,7 +8,7 @@ def pergunta1(df):
     #filtrando coluna
     dfCategory = df[df['Categoria'] == 'Office Supplies']
     
-    #somando coluna e procurando maior indice
+    #somando coluna e procura o indice do maior valor
     agrupamento = dfCategory.groupby('Cidade')['Valor_Venda'].sum().reset_index()
     maiorVenda = agrupamento.loc[agrupamento['Valor_Venda'].idxmax()]
     print('Resposta da Pergunta 1:')
@@ -24,14 +24,30 @@ def pergunta2(df):
     plt.bar(agrupamento['Data_Pedido'], agrupamento['Valor_Venda'], color='green')
     plt.title('Total de Vendas por Data')
     plt.xlabel('Data do Pedido')
-    plt.xticks(rotation=90) #melhor visualização
+    plt.xticks(rotation=45) #melhor visualização
     plt.ylabel('Valor total ($)')
     plt.grid(True)
     plt.tight_layout()
 
     plt.show() 
 
-# df = pd.read_csv('dataset.csv')
+def pergunta3(df):
+    #filtra os dados
+    agrupamento = df.groupby('Estado')['Valor_Venda'].sum().reset_index()
+
+    #plota os dados
+    plt.figure(figsize=(18, 5))
+    plt.bar(agrupamento['Estado'], agrupamento['Valor_Venda'], color='green')
+    plt.title('Total de Vendas por Data')
+    plt.xlabel('Estado')
+    plt.xticks(rotation=90) #melhor visualização
+    plt.ylabel('Valor total ($)')
+    plt.grid(axis='y')
+    plt.tight_layout()
+
+    plt.show()
+
+#df = pd.read_csv('dataset.csv')
 data = pd.read_csv('dataset.csv')
 '''
 exploração de dados (busca de possíveis dados errados[errados ou duplicados])
@@ -40,5 +56,6 @@ print(df[df.duplicated()])
 print(df.isnull().sum())
 '''
 
-pergunta1(data)
-pergunta2(data)
+# pergunta1(data)
+# pergunta2(data)
+pergunta3(data)
